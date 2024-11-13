fca869542131a8238c7e8d31cec7ca9954bf3388
Jilju Joy <jijoy@redhat.com>
2024-11-13 14:35:38 +0530

    test PR to create metallb alone
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/libtest/test_metallb.py
+++ tests/libtest/test_metallb.py
@@ -1,7 +1,5 @@
 import logging
-import pytest
 from ocs_ci.deployment.metallb import MetalLBInstaller
-from ocs_ci.framework import config
 from ocs_ci.framework.pytest_customization.marks import purple_squad, libtest
 
 logger = logging.getLogger(__name__)
@@ -14,21 +12,6 @@ class TestMetalLB(object):
     """
 
     metallb = MetalLBInstaller()
-    config.ENV_DATA.update({"ips_to_reserve": 2})
-
-    @pytest.fixture(autouse=True)
-    def tearDown_fixture(self, request):
-        """
-        Clean up the environment after testing
-        """
-
-        def teardown():
-            """
-            Clean up the environment after testing
-            """
-            self.metallb.undeploy()
-
-        request.addfinalizer(teardown)
 
     @purple_squad
     def test_install_uninstall_metallb(self):
