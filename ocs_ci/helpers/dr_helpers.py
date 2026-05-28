5abb2c2a4e4679eb4fd66c75460a86d4437cab8a
Jilju Joy <jijoy@redhat.com>
2026-05-28 12:45:30 +0530

    add missng name parameter for additionalimages in imagesetconfiguration
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/helpers/dr_helpers.py
+++ ocs_ci/helpers/dr_helpers.py
@@ -3295,8 +3295,8 @@ def generate_rdr_mirror_images():
                 logger.info("Images ready for mirroring in disconnected environment")
             else:
                 logger.warning("No container images found in YAML files")
-            for index, image_url in enumerate(image_list):
-                image_list[index] = image_url[image_url.index("quay") :]
+            # for index, image_url in enumerate(image_list):
+            #     image_list[index] = image_url[image_url.index("quay") :]
             logger.info(image_list)
             return image_list
         else:
